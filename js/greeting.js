83344700c8e1a6dbb2e861ea83c946f640da251a
kimkoo23 <sedreon@gmail.com>
2022-03-19 18:07:54 +0900

    VanilaJS 최종 업데이트

--- js/greeting.js
+++ js/greeting.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.getElementById("greeting");
+const toDo = document.getElementById("todo-form");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -16,6 +17,7 @@ function paingGreetings(username) {
     loginForm.classList.add(HIDDEN_CLASSNAME);
     greeting.innerText = `Hello  ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
+    toDo.classList.remove(HIDDEN_CLASSNAME);
 }
 
 
